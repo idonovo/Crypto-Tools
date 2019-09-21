c132aa3ea03cffc1e038e7f15326c71f27c967c2
Ido Naveh <idonovo@gmail.com>
2019-09-22 01:18:25 +0300

    add homepage

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
 import os
+import glob
 from flask import Flask, flash, request, redirect, url_for, render_template, send_file
 from werkzeug.utils import secure_filename
-import OpenSSL.crypto
-
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234'
-UPLOAD_FOLDER = 'D:/CryptoTools'
+folders = {
+    'UPLOAD_FOLDER': 'D:\CryptoTools\Crypto-Tools\Temp\\',
+    "TSA" : '/RSA_FOLDER'
+}
+
 ALLOWED_EXTENSIONS = set(['pem'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = folders['UPLOAD_FOLDER']
 
 def generate_rsa_sign(key,input, digest):
-    c = OpenSSL.crypto
-    t_key = open(key, 'rt').read()
-    pkey = c.load_privatekey(c.FILETYPE_PEM, t_key)
-    sign =c.sign(pkey, open(input, 'rt').read(), digest)
-    filehandle = open(os.path.dirname(os.path.abspath(input)) + '\output', 'wb')
-    filehandle.write(sign);
+
+    private_key = serialization.load_pem_private_key(
+        key.read(),
+        password=None,
+        backend=default_backend()
+    )
+    hash =  hashes.SHA256() if digest == 'Sha256' else hashes.SHA512()
+    signature = private_key.sign(input.read(),
+                                 padding.PSS( mgf = padding.MGF1(hashes.SHA256()),salt_length = padding.PSS.MAX_LENGTH),
+                                 hash)
+    filehandle = open(os.path.join(app.config['UPLOAD_FOLDER'], 'output'), 'wb')
+    filehandle.write(signature);
     filehandle.close()
     return filehandle
 
@@ -29,9 +42,10 @@ def allowed_file(filename):
 def home_page():
     return render_template('base.html')
 
-
 @app.route('/rsa', methods=['GET', 'POST'])
 def RSA_sign():
+
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files or 'key' not in request.files:
@@ -40,6 +54,7 @@ def RSA_sign():
         input = request.files['file']
         key = request.files['key']
         digest = request.form['DigestSelector']
+        signature_name =  request.form['signature_name'] if not request.form['signature_name'] == '' else 'signature'
 
         # if user does not select file, browser also
         # submit an empty part without filename
@@ -51,11 +66,16 @@ def RSA_sign():
             return redirect(request.url)
 
         inputfilename = secure_filename(input.filename)
-        input.save(os.path.join(app.config['UPLOAD_FOLDER'] +'/RSA_FOLDER', inputfilename))
+        input.save(os.path.join(app.config['UPLOAD_FOLDER'], inputfilename))
         keyfilename = secure_filename(key.filename)
-        key.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/RSA_FOLDER', keyfilename))
-        output = generate_rsa_sign(os.path.join(app.config['UPLOAD_FOLDER'] + '/RSA_FOLDER', keyfilename), os.path.join(app.config['UPLOAD_FOLDER'] +'/RSA_FOLDER', inputfilename), digest)
-        return send_file(output.name)
+        key.save(os.path.join(app.config['UPLOAD_FOLDER'] , keyfilename))
+
+        with open(app.config['UPLOAD_FOLDER'] + keyfilename, "rb") as key:
+            with open(app.config['UPLOAD_FOLDER'] +inputfilename, "rb") as input:
+                result = send_file(generate_rsa_sign(key,input, digest).name, as_attachment=True, attachment_filename=signature_name)
+        os.remove(app.config['UPLOAD_FOLDER'] + keyfilename)
+        os.remove(app.config['UPLOAD_FOLDER'] +inputfilename)
+        return result
     
     return render_template('rsa.html')
 
